706b0013b2da35a2aca42f24916e771dd71c7bcf
DanielleGalong1E <44886287+DanielleGalong1E@users.noreply.github.com>
2018-11-13 10:49:09 +0800

    Update oddEvenTable.java

--- oddEvenTable.java
+++ oddEvenTable.java
@@ -1,9 +1,9 @@
 package javaapplication7;
-public class JavaApplication7 {
+public class oddEvenTable {
     public static void main(String[] args) {
         
         int numbers[]={3, 9, 15, 20, 65, 23, 18, 4, 2, 14, 21};
-        System.out.println("Array Values: ");
+        System.out.print("Array Values: ");
        
         for (int ctr = 0; ctr < numbers.length; ctr++){
             System.out.println(numbers[ctr]+" ");
@@ -20,15 +20,24 @@ public class JavaApplication7 {
                 ctrOdd++;
             }
         }
-        int evenNumbers[] = new int [6];
+        int evenNumbers[] = new int [5];
         int ctrEven = 0;
         
         for(int ctr = 0; ctr < numbers.length; ctr++){
-            if (numbers[ctr]%2 == 1){
+            if (numbers[ctr]%2 == 0){
                 evenNumbers [ctrEven] = numbers[ctr];
                 ctrEven++;
-    }
+            }
+        }
+        for(int ctr = 0; ctr < oddNumbers.length; ctr++){
+            System.out.println(oddNumbers[ctr]);
+        }
+        for(int ctr = 0; ctr < evenNumbers.length; ctr++){
+            System.out.println("\t" + evenNumbers[ctr]);
         }
     }
 }
+
+
+
 
